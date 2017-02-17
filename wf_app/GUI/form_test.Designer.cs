96cd9183cff72529af1d7b398308c34380917841
mivanchenko <mivanchenko@DELLRESERVNB>
2017-02-17 17:01:41 +0300

    Добавьте файлы проекта.

---
+++ wf_app/GUI/form_test.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace wf_app.GUI
+{
+    partial class form_test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._layout_table = new System.Windows.Forms.TableLayoutPanel();
+            this._lvw_select_result = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this._layout_table.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _layout_table
+            // 
+            this._layout_table.ColumnCount = 1;
+            this._layout_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this._layout_table.Controls.Add(this.panel1, 0, 1);
+            this._layout_table.Controls.Add(this._lvw_select_result, 0, 2);
+            this._layout_table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._layout_table.Location = new System.Drawing.Point(0, 0);
+            this._layout_table.Name = "_layout_table";
+            this._layout_table.RowCount = 3;
+            this._layout_table.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this._layout_table.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this._layout_table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this._layout_table.Size = new System.Drawing.Size(597, 274);
+            this._layout_table.TabIndex = 0;
+            // 
+            // _lvw_select_result
+            // 
+            this._lvw_select_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._lvw_select_result.Location = new System.Drawing.Point(3, 109);
+            this._lvw_select_result.Name = "_lvw_select_result";
+            this._lvw_select_result.Size = new System.Drawing.Size(591, 162);
+            this._lvw_select_result.TabIndex = 0;
+            this._lvw_select_result.UseCompatibleStateImageBehavior = false;
+            this._lvw_select_result.View = System.Windows.Forms.View.Details;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(591, 100);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // form_test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 274);
+            this.Controls.Add(this._layout_table);
+            this.Name = "form_test";
+            this.Text = "form_test";
+            this._layout_table.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel _layout_table;
+        private System.Windows.Forms.ListView _lvw_select_result;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ wf_app/GUI/panels/panel_select_criteria.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using wf_app.resources;
+
+namespace wf_app.GUI.panels
+{
+    public class panel_select_criteria : Panel
+    {
+        /*
+         * --------------------------------------------------------------------
+         *                          CONSTRUCTION
+         * --------------------------------------------------------------------
+         */
+        #region __CONSTRUCTION__
+
+        public panel_select_criteria()
+        {
+            InitializeComponent();
+        }
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #endregion //__CONSTRUCTION__
+
+        /*
+         * --------------------------------------------------------------------
+         *                          INITIALIZE
+         * --------------------------------------------------------------------
+         */
+        #region __INITIALIZE__
+        /// <summary>
+        /// create_form_elements( )
+        /// </summary>
+        private void create_form_elements()
+        {
+            this._layout_table = new System.Windows.Forms.TableLayoutPanel();
+
+            this._x_subject = new System.Windows.Forms.TextBox();
+            this._x_regime = new System.Windows.Forms.TextBox();
+            this._x_region = new System.Windows.Forms.TextBox();
+            this._x_fish = new System.Windows.Forms.TextBox();
+            this._x_declarant = new System.Windows.Forms.TextBox();
+        }
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.create_form_elements();
+
+            this._layout_table.SuspendLayout();
+            this.SuspendLayout();
+            //
+            //_layout_table
+            //
+            this.init_layout( );
+            //
+            //_x_subject
+            //
+            this.init_subject( );
+            //
+            //_x_regime
+            //
+            this.init_regime( );
+            //
+            //_x_region
+            //
+            this.init_region( );
+            //
+            //_x_fish
+            //
+            this.init_fish( );
+            //
+            //_x_declarant
+            //
+            this.init_declarant( );
+            //--
+            this._layout_table.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+        /// <summary>
+        /// init_layout( )
+        /// </summary>
+        private void init_layout()
+        {
+            this._layout_table.Name = "_layout_table";
+            this._layout_table.ColumnCount = panel_select_criteria._LAYOUT_COLS_;
+            for( int i = 0; i < panel_select_criteria._LAYOUT_COLS_; ++i )
+            {
+                this._layout_table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            }
+            this._layout_table.RowCount = panel_select_criteria._LAYOUT_ROWS_;
+            for (int i = 0; i < panel_select_criteria._LAYOUT_ROWS_; ++i)
+            {
+                this._layout_table.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            }
+            this._layout_table.Dock = DockStyle.Fill;
+            this._layout_table.TabIndex = 0;
+        }
+        /// <summary>
+        /// init_subject( )
+        /// </summary>
+        private void init_subject( )
+        {
+            System.Windows.Forms.Label lbl = new Label();
+            lbl.Text = resource_panel_search_criteria._x_subject;
+        }
+        /// <summary>
+        /// init_regime( )
+        /// </summary>
+        private void init_regime( )
+        {
+
+        }
+        /// <summary>
+        /// init_region( )
+        /// </summary>
+        private void init_region( )
+        {
+
+        }
+        /// <summary>
+        /// init_fish( )
+        /// </summary>
+        private void init_fish( )
+        {
+
+        }
+        /// <summary>
+        /// init_declarant( )
+        /// </summary>
+        private void init_declarant( )
+        {
+
+        }
+        #endregion //__INITIALIZE__
+
+        /*
+         * --------------------------------------------------------------------
+         *                          FUNCTIONS
+         * --------------------------------------------------------------------
+         */
+        #region __FUNCTIONS__
+        #endregion//__FUNCTIONS__
+
+        /*
+         * --------------------------------------------------------------------
+         *                          FIELDS
+         * --------------------------------------------------------------------
+         */
+        #region __FIELDS__
+        private const int _LAYOUT_COLS_ = 4;
+        private const int _LAYOUT_ROWS_ = 5;
+
+        private const int _ROW_SUBJECT_ = 0;
+        private const int _ROW_REGIME_ = 1;
+        private const int _ROW_REGION_ = 2;
+        private const int _ROW_FISH_ = 3;
+        private const int _ROW_DECLARANT_ = 4;
+
+        private const int _COL_LABEL_ = 0;
+        private const int _COL_TEXT_ = 0;
+        private const int _COL_SELECT_ = 0;
+        private const int _COL_CLEAR_ = 0;
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        //
+        private System.Windows.Forms.TableLayoutPanel _layout_table;
+        private System.Windows.Forms.TextBox _x_subject;
+        private System.Windows.Forms.TextBox _x_regime;
+        private System.Windows.Forms.TextBox _x_region;
+        private System.Windows.Forms.TextBox _x_fish;
+        private System.Windows.Forms.TextBox _x_declarant;
+        //
+        #endregion//__FIELDS__
+
+    }//public class panel_select_criteria : Panel
+
+}//namespace wf_app.GUI.panels
